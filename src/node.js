b760f067f57654531ad1e285443f0c6fd76302a7
Abzal <Abzal.Tankibayev@outlook.com>
2019-10-17 09:32:41 +0600

    feat: add task

--- src/node.js
+++ src/node.js
@@ -1,22 +1,58 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
-	appendChild(node) {
-
+	appendChild(node) {				
+		!this.left ? (this.left = node) : (!this.right && (this.right = node));
+		node.parent = this;	
 	}
 
-	removeChild(node) {
-
+	removeChild(node) {		
+		switch(node) {
+			case this.left :
+					[this.left, node.parent] = [null, null];
+					break;
+			case this.right :
+					[this.right, node.parent] = [null, null];					
+					break;
+			default : error();						
+		}
 	}
 
 	remove() {
-
+		this.parent && this.parent.removeChild(this);
 	}
 
 	swapWithParent() {
-		
+		if (this.parent) {
+			if (this.left) this.left.parent = this.parent;
+			if (this.right) this.right.parent = this.parent;
+
+			if (this === this.parent.left) {
+				if (this.parent.right) this.parent.right.parent = this;
+				[this.parent.left, this.parent.right, this.left, this.right] = [this.left, this.right, this.parent, this.parent.right];
+			}
+
+			if (this === this.parent.right) {
+				if (this.parent.left) this.parent.left.parent = this;
+				[this.parent.left, this.parent.right, this.left, this.right] = [this.left, this.right, this.parent.left, this.parent];
+			}
+
+			if (this.parent.parent) {
+				if (this.parent === this.parent.parent.left) {
+					this.parent.parent.left = this;
+				}
+				if (this.parent === this.parent.parent.right) {
+					this.parent.parent.right = this;
+				}
+			}
+			[this.parent.parent, this.parent] = [this, this.parent.parent];
+		}
 	}
 }
 
--- src/queue.js
+++ src/queue.js
@@ -1,24 +1,27 @@
 const MaxHeap = require('./max-heap.js');
+const error = () => {throw Error;}	
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+		this.defaultMaxSize = 30;
+		this.maxSize = maxSize || this.defaultMaxSize;
+		this.heap = new MaxHeap;
 	}
 
-	push(data, priority) {
-
+	push(data, priority) {	
+		this.size() === this.maxSize ? error() : this.heap.push(data, priority);		
 	}
 
 	shift() {
-
+		return this.isEmpty() ? error() : this.heap.pop();
 	}
 
 	size() {
-
+		return this.heap.size();
 	}
 
 	isEmpty() {
-		
+		return this.heap.isEmpty();
 	}
 }
 
